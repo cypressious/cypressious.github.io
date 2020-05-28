094dfdc5d204b3cf430631e499ac29244808aff0
Kirill Rakhman <kirill.rakhman@gmail.com>
2020-05-28 22:33:08 +0200

    add redirects to xing and linkedin

---
+++ src/pages/Linkedin.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Redirecting</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Linkedin extends Vue {
+    mounted() {
+        window.location.href = 'https://www.linkedin.com/in/kirillrakhman/'
+    }
+}
+</script>
---
+++ src/pages/Xing.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Redirecting</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LinkedIn extends Vue {
+    mounted() {
+        window.location.href = 'https://www.xing.com/profile/Kirill_Rakhman/cv'
+    }
+}
+</script>
