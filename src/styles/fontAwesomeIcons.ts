113cc5e89b0896dfae749f2b264d5761441a6c6b
Kirill Rakhman <kirill.rakhman@gmail.com>
2020-07-20 12:30:24 +0200

    add sudoku solver project

--- src/styles/fontAwesomeIcons.ts
+++ src/styles/fontAwesomeIcons.ts
@@ -12,10 +12,10 @@ import {
     faTwitter,
     faXing
 } from '@fortawesome/free-brands-svg-icons';
-import { faArrowLeft, faCircle, faEye, faFilePdf, faVideo } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faCircle, faEye, faFilePdf, faGlobe, faVideo } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
     faCircle, faEye, faFilePdf, faVideo, faArrowLeft,
     faGithub, faStackOverflow, faTwitter, faLinkedin, faXing, faGooglePlay, faJava, faAndroid, faMicrosoft, faHtml5,
-    faFirefoxBrowser
+    faFirefoxBrowser, faGlobe
 );
