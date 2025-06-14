58f010f67db7f795c59d8e76dd2e329758df67a6
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-06-14 11:44:26 +0200

    Add Privacy Policy for Recipe Plan

---
+++ src/pages/recipe-plan/PrivacyPolicy.vue
@@ -0,0 +1,19 @@
+<template>
+    <Layout>
+        <h1 class="title">Recipe Plan Privacy Policy</h1>
+
+        <p>Recipe Plan does not collect, transmit, distribute or sell your data.</p>
+
+        <p>All data is saved locally on the device and in the Google Sheet configured in the app.</p>
+
+        <p>For Google's Privacy Policy, visit <a href="https://policies.google.com/privacy" target="_blank">https://policies.google.com/privacy</a>.</p>
+    </Layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PrivacyPolicy extends Vue {
+}
+</script>
